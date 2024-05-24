459f739c7fb5d7f64476ce3429f0f721c4ea8871
Jeason <me@cowpoke.cc>
2024-05-24 23:27:42 +0800

    fix: host

--- app/preview/[slug]/page.tsx
+++ app/preview/[slug]/page.tsx
@@ -2,6 +2,8 @@
 // import Head from 'next/head';
 import { Metadata } from 'next';
 
+const host = 'https://farcaster-frame-inscriptions.vercel.app';
+
 export async function generateMetadata({
   params,
 }: {
@@ -13,22 +15,22 @@ export async function generateMetadata({
   const fcMetadata: Record<string, string> = {
     'fc:frame': 'vNext',
     // 'fc:frame:post_url': `${process.env['HOST']}/api/vote?id=${id}`,
-    'fc:frame:image': `/preview/${slug}/opengraph-image`,
+    'fc:frame:image': `${host}/preview/${slug}/opengraph-image`,
     'fc:frame:button:1': 'Create My Own',
     'fc:frame:button:1:action': 'link',
-    'fc:frame:button:1:target': process.env['HOST']!,
+    'fc:frame:button:1:target': host,
   };
 
   return {
     title: 'farcaster',
     openGraph: {
       title: 'farcaster',
-      images: [`/preview/${slug}/opengraph-image`],
+      images: [`${host}/preview/${slug}/opengraph-image`],
     },
     other: {
       ...fcMetadata,
     },
-    // metadataBase: new URL('/'),
+    metadataBase: new URL(host),
   };
 }
 
